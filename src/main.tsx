5803a18b1eea3290562482657f57316d84e3b8aa
hoppp666 <valeronjohn43@gmail.com>
2026-02-13 07:41:45 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import { App } from "./App";
+import "https://github.com/hoppp666/pf/blob/main/src/index.css";
+import { App } from "https://github.com/hoppp666/pf/blob/main/src/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
